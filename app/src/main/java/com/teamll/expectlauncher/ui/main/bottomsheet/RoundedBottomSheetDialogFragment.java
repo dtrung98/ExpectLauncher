8a317e17e23b5c924f36e97796d8c5b5536e0941
NguyenHuuTu <1612772@student.hcmus.edu.vn>
2018-11-20 19:05:30 +0700

    draft

--- app/src/main/java/com/teamll/expectlauncher/ui/main/bottomsheet/RoundedBottomSheetDialogFragment.java
+++ app/src/main/java/com/teamll/expectlauncher/ui/main/bottomsheet/RoundedBottomSheetDialogFragment.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 
 import com.teamll.expectlauncher.R;
